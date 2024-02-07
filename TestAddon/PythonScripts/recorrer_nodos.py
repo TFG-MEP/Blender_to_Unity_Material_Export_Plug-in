66f763a28f1d7152aaa1efcd3b6047aae4b59c0b
elisatodd <elitodd@ucm.es>
2024-02-07 18:46:48 +0100

    Nuevo recorrido de nodos y generación de shader acorde

---
+++ TestAddon/PythonScripts/recorrer_nodos.py
@@ -0,0 +1,324 @@
+import bpy
+
+added_functions = set()
+
+class NodeInfo:
+    def __init__(self, name="", nodeType=""):
+        self.name = name
+        self.nodeType = nodeType
+
+# Dado un valor de propiedad de un tipo concreto en blender, devuelve un
+# string con el formato adecuado en HLSL
+def convertir_valor(blender_input, input_type) : 
+    if (input_type == 'Float') :
+        return blender_input
+    elif (input_type == 'Color') :
+        return f'fixed4({blender_input[0]},{blender_input[1]},{blender_input[2]}, 1.0)'
+    elif(input_type == 'Vector') :
+        return f'fixed3({blender_input[0]}, {blender_input[1]}, {blender_input[2]})'
+    else : 
+        return blender_input
+
+# Convierte un string que determina un tipo de datos usado por blender,
+# en un string con el tipo de datos equivalente en HLSL
+def convertir_tipos(blender_input):
+    if blender_input == 'Float':
+        output = 'float'
+    elif blender_input == 'Color' :
+        output = 'fixed4'
+    elif blender_input == 'Vector' :
+        output = 'fixed3'
+    else : 
+        output = blender_input
+    # TO_DO : completar con todos los tipos de datos
+    return output
+
+"""
+    Añade un método HLSL y su llamada en el shader.
+
+    Parameters
+    ----------
+    function_file_path : str
+        Ruta del archivo HLSL que contiene el método.
+    function_parameters : list
+        Lista de parámetros del nodo, o parámetros de llamada a la función.
+    destination_node : bpy.types.Node
+        Nodo de destino donde se conectará la salida del nodo actual.
+    destination_property : bpy.types.NodeSocket
+        Propiedad de destino donde se conectará la salida del nodo actual.
+    shader_content : str
+        Contenido del shader.
+
+    Returns
+    -------
+    shader_content : str
+        Contenido del shader actualizado con el método HLSL y su llamada.
+"""
+def escribir_nodo(function_file_path, function_parameters, destination_node, destination_property, shader_content) : 
+
+    global added_functions
+
+    # Comprobar si la función HLSL ya se ha agregado al shader
+    if function_file_path not in added_functions:
+
+        # Se lee el archivo donde está el método HLSL para este tipo de nodo
+        with open(function_file_path, "r") as node_func_file:
+            node_function = node_func_file.read()
+
+        # Añadir la función en la sección de la plantilla indicada para ello
+        methods_index = shader_content.find("// Add methods")
+        shader_content = shader_content[:methods_index] + node_function + "\n\t\t\t" + shader_content[methods_index:]
+
+        # Actualizar el conjunto de funciones HLSL escritas
+        added_functions.add(function_file_path)
+
+    # Quitar espacios a los nombres
+    destination_node = destination_node.name.replace(" ", "")
+    destination_property = destination_property.name.replace(" ", "")
+    # Obtenemos el nombre del archivo sin extensión (debe coincidir con el nombre de la función)
+    dot_index = function_file_path.find('.')
+    function_name = function_file_path[:dot_index]
+
+    # Añadir la llamada a la función en el fragment shader
+    fragment_index = shader_content.find("// Call methods")
+
+    # El resultado de la llamada a esta función se asigna a lo que indique su conexión
+    destination_name = destination_node + "_" + destination_property
+
+    all_parameters = ', '.join(function_parameters)
+
+    func_line = f'{destination_name} = {function_name}({all_parameters});\n\t\t\t\t'
+    shader_content = shader_content[:fragment_index] + func_line + shader_content[fragment_index:]
+
+    return shader_content
+
+"""
+    Procesa una propiedad de un nodo de blender y las añade al shader
+    en un formato adaptado.
+
+    Parameters
+    ----------
+    input_socket : bpy.types.NodeSocket
+        Propiedad del nodo a procesar.
+    nodeName : str
+        Nombre del nodo.
+    nodeType : str
+        Tipo del nodo.
+    shader_content : str
+        Contenido del shader.
+
+    Returns
+    -------
+    shader_content : str
+        Contenido del shader actualizado con la propiedad procesada.
+"""
+def procesar_propiedad(input_socket, nodeName, nodeType, shader_content): 
+    
+    propName = input_socket.name
+    propLabel = input_socket.bl_label
+
+    # Ignorar propiedades de tipo "Shader"
+    if propLabel == 'Shader' : # Las propiedades de tipo "shader" vienen dadas por un cálculo con otro nodo
+        return shader_content
+
+    # Ignorar propiedades específicas de ciertos tipos de nodos
+    if nodeType == 'BSDF_PRINCIPLED' :
+        if propName == 'IOR' : # Por ejemplo, si el IOR no lo usamos, lo ignoramos aquí
+            return shader_content
+    elif nodeType == 'OUTPUT_MATERIAL' : 
+        # No escribimos ninguna propiedad de output, solo nos interesa lo que haya conectado a Surface
+        return shader_content
+
+    # Convertir el tipo de blender a tipos de Unity/HLSL
+    propLabel = convertir_tipos(propLabel)
+
+    # Sacamos el valor de la propiedad
+    propValue = input_socket.default_value
+
+    # TO_DO : operaciones con el valor si es necesario: normalizar, convertir a valores que entienda el shader, etc.
+    propValue = convertir_valor(propValue, input_socket.bl_label)
+
+    # Quitar espacios a los nombres
+    nodeName = nodeName.replace(" ", "")
+    propName = propName.replace(" ", "")
+
+    # Añadir la propiedad a la plantilla
+    # Lo que se escribe es:
+    # nombreDelNodo_nombreDeLaPropiedad("nombreDeLaPropiedad", TipoDeDatos) = ValorDeLaPropiedad
+    property_line = f'{nodeName}_{propName}("{propName}", {propLabel}) = {propValue}\n\t\t'
+    shader_content = escribir_propiedad(property_line, shader_content)
+
+    return shader_content
+
+"""
+    Agrega una línea a la sección de propiedades del shader.
+
+    Parameters
+    ----------
+    line : str
+        Línea con la propiedad a escribir.
+    shader_content : str
+        Contenido actual del shader.
+
+    Returns
+    -------
+    shader_content : str
+        Contenido del shader actualizado con la nueva propiedad.
+"""
+def escribir_propiedad(line, shader_content) : 
+    properties_index = shader_content.find("// Add properties")
+    shader_content = shader_content[:properties_index] + line + shader_content[properties_index:]
+    return shader_content
+
+
+"""
+    Añade un nodo de tipo "Value" al shader.
+
+    Parameters
+    ----------
+    node : bpy.types.Node
+        Nodo de tipo "Value" a agregar en la concatenación de llamadas.
+    node_properties : list
+        Lista de propiedades del nodo.
+    shader_content : str
+        Contenido del shader.
+
+    Returns
+    -------
+    shader_content : str
+        Contenido del shader actualizado con el nodo "Value".
+"""
+def escribir_nodo_value(node, node_properties, shader_content) :
+
+    # Se buscan las propiedades específicas de este tipo de nodo...
+    node_name = node.name.replace(" ", "")
+    node_properties.append(node_name + "_Value")
+    property_line = f'{nodeInfo.name}_Value("Value", float) = {node.value}\n\t\t'
+    # ... y se añaden al shader
+    shader_content = escribir_propiedad(property_line, shader_content)
+
+    # Se identifica el nodo conectado a la salida Value
+    conexion_salida = node.outputs["Value"].links[0]
+    nodo_entrada = conexion_salida.to_node
+    # y la propiedad específica de dicho nodo que lo recibe
+    propiedad_entrada = conexion_salida.to_socket
+    #print("Value conecta con " + nodo_entrada.name + " en su propiedad " + propiedad_entrada.name)
+    shader_content = escribir_nodo("value.txt", node_properties, nodo_entrada, propiedad_entrada, shader_content)
+
+    return shader_content
+
+def escribir_nodo_rgb(node, node_properties, shader_content) : 
+
+    # Se buscan las propiedades específicas de este tipo de nodo...
+    node_name = node.name.replace(" ", "")
+    node_properties.append(node_name + "_Color")
+
+    node_color = procesar_propiedad(node.color, "Color")
+
+    property_line = f'{node_name}_Color("Color", fixed4) = {node_color}\n\t\t'
+    # ... y se añaden al shader
+    shader_content = escribir_propiedad(property_line, shader_content)
+
+    # Se identifica el nodo conectado a la salida RGB
+    conexion_salida = node.outputs["Color"].links[0]
+    nodo_entrada = conexion_salida.to_node
+    # y la propiedad específica de dicho nodo que lo recibe
+    propiedad_entrada = conexion_salida.to_socket
+    #print("RGB conecta con " + nodo_entrada.name + " en su propiedad " + propiedad_entrada.name)
+    shader_content = escribir_nodo("rgb.txt", node_properties, nodo_entrada, propiedad_entrada, shader_content)
+
+    return shader_content
+
+def escribir_nodo_bsdf(node, node_properties, shader_content) :
+
+    conexion_salida = node.outputs["BSDF"].links[0]
+    nodo_entrada = conexion_salida.to_node
+    propiedad_entrada = conexion_salida.to_socket
+
+    shader_content = escribir_nodo("principled_bsdf.txt", node_properties, nodo_entrada, propiedad_entrada, shader_content)
+
+    return shader_content
+
+"""
+    Recorre los nodos del material y los agrega al shader.
+
+    Parameters
+    ----------
+    node : bpy.types.Node
+        Nodo a procesar.
+    shader_content : str
+        Contenido del shader.
+
+    Returns
+    -------
+    shader_content : str
+        Contenido del shader actualizado con los nodos procesados.
+"""
+def recorrer_nodo(node, shader_content):
+    nodeInfo = NodeInfo(name=node.name.replace(" ", ""), nodeType=node.type.replace(" ", ""))
+    print ("Recorriendo nodo: " + node.name + "\n")
+    
+    node_properties = []
+
+    # Se recorren las propiedades o inputs del nodo
+    for input_socket in node.inputs :
+
+        # Se guarda cada propiedad leída
+        socket_name = input_socket.name.replace(" ", "")
+        node_properties.append(nodeInfo.name + "_" + socket_name)
+
+        if input_socket.is_linked : # Si están conectados a otro nodo, se recorre este
+            # input_socket.links[0] indica que solo trabajamos con una conexión 
+            # por cada propiedad/input. Si por algún motivo hubiera más (en principio 
+            # blender no permite hacer eso para los nodos con los que estamos trabajando), 
+            # solo trabajamos con la primera. Si más adelante utilizamos nodos con múltiples
+            # conexiones, habría que revisitar este apartado (pero esto no es un uso común).
+            connected_node = input_socket.links[0].from_node
+            shader_content = recorrer_nodo(connected_node, shader_content)
+        else :
+            # Crear en las propiedades del shader, una entrada para esta propiedad
+            print("Property of " + nodeInfo.nodeType + ": " + input_socket.name + " with type: " + input_socket.bl_label)
+            shader_content = procesar_propiedad(input_socket, nodeInfo.name, nodeInfo.nodeType, shader_content)
+
+    # TO_DO quizás es mejor recorrer las salidas aquí
+
+    if node.type == 'VALUE':
+        shader_content = escribir_nodo_value(node, node_properties, shader_content)
+    elif node.type == 'RGB' : 
+        shader_content = escribir_nodo_rgb(node, node_properties, shader_content)
+    elif node.type == 'BSDF_PRINCIPLED' :
+        shader_content = escribir_nodo_bsdf(node, node_properties, shader_content)
+
+    return shader_content
+
+
+def start():
+    # Cargar la plantilla .shader
+    template_shader_path = "template.shader"
+
+    with open(template_shader_path, "r") as template_file:
+        template_shader = template_file.read()
+
+    # Crear una copia de la plantilla para trabajar en ella
+    shader_content = template_shader
+            
+    selected_object = bpy.context.active_object
+    material = selected_object.active_material
+    material.use_nodes = True
+    node_tree = material.node_tree
+    nodes = node_tree.nodes
+    root_node = nodes.get("Material Output")
+
+    # Cambiar el nombre del shader
+    shader_content = shader_content.replace("Custom/ColorShader", f"Custom/Shader{material.name}_")
+
+    shader_filename = f"{material.name}_.shader"
+    shader_filepath = "" + shader_filename
+
+    shader_content = recorrer_nodo(root_node, shader_content)
+
+    with open(shader_filepath, "w") as shader_file:
+        shader_file.write(shader_content)
+
+    print(f"Archivo {shader_filename} generado con éxito.")
+    print("Proceso completado.")
