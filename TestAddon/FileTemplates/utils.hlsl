7ae2b2c38483641715476cc09c1fa3db08599a9e
MiriamMartín <mirima10@ucm.es>
2024-03-12 23:48:17 +0100

    Update utils.hlsl

--- TestAddon/FileTemplates/utils.hlsl
+++ TestAddon/FileTemplates/utils.hlsl
@@ -24,17 +24,17 @@ float4 float_to_float4(float val){
     return float4(val, val, val, val);
 }
 float3 float_to_float3(float val){
-    return float3(val,1,1);
+    return float3(val,val,val);
 }
 float2 float_to_float2(float val){
     return float2(val, val);
 }
 
-float4 float2_to_float4(float2 vec, float z, float w){
-    return float4(vec.x, vec.y, z, w);
+float4 float2_to_float4(float2 vec){
+    return float4(vec.x, vec.y, 1, 1);
 }
-float3 float2_to_float3(float2 vec, float z){
-    return float3(vec.x, vec.y, z);
+float3 float2_to_float3(float2 vec){
+    return float3(vec.x, vec.y,1);
 }
 float float2_to_float(float2 vec){
     return (vec.x + vec.y) / 2.0;
