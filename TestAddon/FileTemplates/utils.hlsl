d580a80e5f079581e86f77ab6cd836493baf0127
MiriamMartín <mirima10@ucm.es>
2024-03-12 23:39:40 +0100

    funciones utils

---
+++ TestAddon/FileTemplates/utils.hlsl
@@ -0,0 +1,54 @@
+float3 float3_to_float4(float3 vec){
+    return float4(vec,1);
+}
+float2 float3_to_float2(float3 vec){
+    return vec.xy;
+}
+float float3_to_float(float3 vec){
+    return (vec.x + vec.y + vec.z) / 3.0;
+}
+
+
+float4 float4_to_float3(float4 vec){
+    return vec.xyz;
+}
+float2 float4_to_float2(float4 vec){
+    return vec.xy;
+}
+float float4_to_float(float4 vec){
+    return (vec.x + vec.y + vec.z + vec.w) / 4.0;
+}
+
+
+float4 float_to_float4(float val){
+    return float4(val, val, val, val);
+}
+float3 float_to_float3(float val){
+    return float3(val,1,1);
+}
+float2 float_to_float2(float val){
+    return float2(val, val);
+}
+
+float4 float2_to_float4(float2 vec, float z, float w){
+    return float4(vec.x, vec.y, z, w);
+}
+float3 float2_to_float3(float2 vec, float z){
+    return float3(vec.x, vec.y, z);
+}
+float float2_to_float(float2 vec){
+    return (vec.x + vec.y) / 2.0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
