07d806c48db13232e204b5526caf0f8fe27d6cf5
PaulaMorillasAlonso <pamorill@ucm.es>
2024-03-13 21:25:55 +0100

    STRUCT RGB

--- TestAddon/Strategies/RGB_strategy.py
+++ TestAddon/Strategies/RGB_strategy.py
@@ -18,10 +18,19 @@ class RGBNode(Strategy):
         variable_line = f'float4 {node_name}_Color;\n\t\t\t'
         shader_content = write_variable(variable_line, shader_content)
 
-        for link in node.outputs["Color"].links :
-            input_node = link.to_node
+        # Add the struct (TODO : llevar la cuenta de structs ya añadidos)
+        shader_content = write_struct("HLSLTemplates/RGB/struct.txt", shader_content)
+
+        # Add the function to the shader template
+        shader_content = write_function("HLSLTemplates/RGB/rgb.txt", shader_content)
+
+        all_parameters = ', '.join(node_properties)
+        shader_content = write_struct_node(node_name, "RGB", "rgb", all_parameters, shader_content)
+        
+        for exit_connection in node.outputs["Color"].links :
+            input_node = exit_connection.to_node
             # y la propiedad específica de dicho nodo que lo recibe
-            input_property = link.to_socket
-            shader_content = write_node("HLSLTemplates/rgb.txt", node_properties, input_node, input_property, shader_content)
+            input_property = exit_connection.to_socket
+            shader_content = write_struct_property(node_name, "Color", "float4", input_node, input_property, shader_content)
 
         return shader_content
