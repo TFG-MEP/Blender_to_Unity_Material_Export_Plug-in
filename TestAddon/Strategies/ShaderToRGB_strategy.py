905724d27492f0e8ff246359f9ba3bb2a68331ca
PaulaMorillasAlonso <pamorill@ucm.es>
2024-03-19 11:18:25 +0100

    shader to rgb struct

--- TestAddon/Strategies/ShaderToRGB_strategy.py
+++ TestAddon/Strategies/ShaderToRGB_strategy.py
@@ -7,10 +7,28 @@ class ShaderToRgbNode(Strategy):
         node_name = node.name.replace(" ", "")
         node_name=node_name.replace(".", "")
 
-        for link in node.outputs["Color"].links :
-            input_node = link.to_node
-            # y la propiedad específica de dicho nodo que lo recibe
-            input_property = link.to_socket
-            shader_content = write_node("HLSLTemplates/shader_to_RGB.txt", node_properties, input_node, input_property, shader_content)
+         # Add the struct (TODO : llevar la cuenta de structs ya añadidos)
+        shader_content = write_struct("HLSLTemplates/Shader_to_RGB/struct.txt", shader_content)
+
+        # Add the function to the shader template
+        shader_content = write_function("HLSLTemplates/Shader_to_RGB/shader_to_RGB.txt", shader_content)
+        
+        # Add the function call to the shader template
+        all_parameters = ', '.join(node_properties)
+        shader_content = write_struct_node(node_name, "Shader_to_RGB", "shader_to_RGB", all_parameters, shader_content)
+
+
+        for exit_connection in node.outputs["Color"].links  :
+            input_node = exit_connection.to_node
+            input_property = exit_connection.to_socket
+
+            shader_content = write_struct_property(node_name, "Color", "float3", input_node, input_property, shader_content)
+
+        for exit_connection in node.outputs["Alpha"].links :
+            input_node = exit_connection.to_node
+            input_property = exit_connection.to_socket
+
+            shader_content = write_struct_property(node_name, "Alpha", "float", input_node, input_property, shader_content)
+           
 
         return shader_content
