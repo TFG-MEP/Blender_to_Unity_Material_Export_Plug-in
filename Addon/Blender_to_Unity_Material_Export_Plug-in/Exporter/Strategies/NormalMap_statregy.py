bfa6eb957579b2e5668e03fa577d0b729b16e71e
elisatodd <elitodd@ucm.es>
2024-04-10 13:05:12 +0200

    Refactorizacion structs de Normal Map

--- Addon/Blender_to_Unity_Material_Export_Plug-in/Exporter/Strategies/NormalMap_statregy.py
+++ Addon/Blender_to_Unity_Material_Export_Plug-in/Exporter/Strategies/NormalMap_statregy.py
@@ -4,21 +4,10 @@ from ..writing_utils import *
 class NormalMapNode(Strategy):
 
     function_path = "HLSLTemplates/Normal_Map/normal_map.txt"
-    struct_path = "HLSLTemplates/Normal_Map/struct.txt"
+    function_name = "normal_map"
 
     def add_custom_properties(self, node, node_properties, shader_content):
         return node_properties, shader_content
-    
-    def add_struct(self, node, node_properties, shader_content):
-        shader_content = write_struct(self.struct_path, shader_content)
-        all_parameters = ', '.join(node_properties)
-        shader_content = write_struct_node(self.node_name(node), "Normal_map", "normal_map", all_parameters, shader_content)
-
-        return shader_content
-   
-    def add_function(self, node, node_properties, shader_content):
-        shader_content = write_function(self.function_path, shader_content)
-        return shader_content
     
     def write_outputs(self, node, node_properties, shader_content) :
         node_name = self.node_name(node)
